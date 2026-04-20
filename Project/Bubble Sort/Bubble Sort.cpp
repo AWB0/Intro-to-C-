9060f1941409d3005dc4397738291afd70b3ba94
AWB0 <theawb0@gmail.com>
2026-04-20 23:08:10 +1000

    Bubble Sort Commit 1
    added Initial Test Functions

--- Project/Bubble Sort/Bubble Sort.cpp
+++ Project/Bubble Sort/Bubble Sort.cpp
@@ -3,14 +3,49 @@
 #include <iostream>
 
 
-int Function()
+
+void print_array(int array[], int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << array[i] << ",\t";
+        if (i % 10 == 9) std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+}
+//swaps the contents of the first and second integers
+void swap(int& first, int& second)
+{
+    int temp = first;
+    first = second;
+    second = temp;
+}
+
+int BubbleSort(int array[], int toSort)
 {
 
-
-
-
-
+    //exit for single element or invalid indices
+    if (toSort < 2) 
+    {
+        return;
+    }
+    //Value to Track if List is Sorted or not
+    bool sorted = false; //false to ensure the loop begins
+    while (!sorted)
+    {
+        sorted = true;
+    }
+    for (int i = 0; i < toSort - i; ++i) 
+    {
+        if (array[i] > array[i + 1])
+        {
+            swap(array[i], array[i + 1]);
+          sorted = false;
+        }
+    }
 }
+
 int main()
 {
 
