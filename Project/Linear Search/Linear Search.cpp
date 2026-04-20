6f21b166b1a41397128bbfc62cf7df3180c38e45
AWB0 <theawb0@gmail.com>
2026-04-20 20:40:19 +1000

    Commit Linear Search function

--- Project/Linear Search/Linear Search.cpp
+++ Project/Linear Search/Linear Search.cpp
@@ -1,22 +1,40 @@
 #pragma once
 #include <string.h>
 #include <iostream>
+#include <vector>
 
 
 
 
 
-int Function()
+
+int Function(int numberToFind)
 {
-	
-	
-	
 
 
+	std::vector<int> array;
+	for (int i = 1; i <= 1000; i++) 
+	{
+		array.push_back(i);
+	}
+	
+	int loops = 0;
+
+	// loop through the array based on its size linearly
+	for (int i = 0; i < array.size(); i++) 
+	{
+
+		// when value has been found
+		if (array[i] == numberToFind) 
+		{
+			std::cout << "found Value: " << i;
+		}
+	}
+	
+
+	return 0;
 }
 int main()
 {
-
 	
-
 }
