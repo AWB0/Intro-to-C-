23d2e12efa168b52c3d568833903c99b59a9b76f
AWB0 <theawb0@gmail.com>
2026-04-22 21:24:00 +1000

    added more functionality
    solution is merely functional and probably not exactly ideal

--- Project/FizzBuzz/FizzBuzz.cpp
+++ Project/FizzBuzz/FizzBuzz.cpp
@@ -8,8 +8,8 @@ int FizzBuzzFunction(int integer)
 {
 	int multiple3 = 0;
 	int multiple5 = 0;
-	
-	for (int i = 0; i < integer; i++) 
+
+	for (int i = 0; i < integer; i++)
 	{
 		multiple5 = i * 5;
 		if (multiple5 == integer)
@@ -26,18 +26,42 @@ int FizzBuzzFunction(int integer)
 
 	for (int i = 0; i < integer; i++)
 	{
-		
+
 		multiple3 = i * 3;
 		if (multiple3 == integer)
 		{
 			break;
 		}
-		else if (multiple3 > integer) 
+		else if (multiple3 > integer)
 		{
 			break;
 		}
 
 	}
+
+	if (integer == multiple3 && integer == multiple5)
+	{
+		std::cout << "FizzBuzz" << "\n";
+	}
+
+	else if (integer == multiple3)
+	{
+		std::cout << "Fizz" << "\n";
+	}
+
+	else if (integer == multiple5)
+	{
+		std::cout << "Buzz " << "\n";
+	}
+	
+	else
+	{
+		std::cout << integer << "\n";
+	}
+
+	return 0;
+
+}
 
 
 	
