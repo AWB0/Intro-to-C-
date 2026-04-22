87748c92988c19ef383ce2720dee0762e7834e2c
AWB0 <theawb0@gmail.com>
2026-04-22 21:21:20 +1000

    Implement Function FizzBuzz Temporary Logic

--- Project/FizzBuzz/FizzBuzz.cpp
+++ Project/FizzBuzz/FizzBuzz.cpp
@@ -8,7 +8,7 @@ int FizzBuzzFunction(int integer)
 {
 	int multiple3 = 0;
 	int multiple5 = 0;
-	// loop through integers until we reach the current integer and check if its equal to a multiple of 5
+	
 	for (int i = 0; i < integer; i++) 
 	{
 		multiple5 = i * 5;
@@ -23,7 +23,7 @@ int FizzBuzzFunction(int integer)
 	}
 
 
-	// loop through integers until we reach the current integer and check if its equal to a multiple of 3, if so break
+
 	for (int i = 0; i < integer; i++)
 	{
 		
@@ -40,37 +40,4 @@ int FizzBuzzFunction(int integer)
 	}
 
 
-	//if integer is a multiple of 3 and 5 send FIZZBUZZ
-	if (integer == multiple3 && integer == multiple5)
-	{
-		std::cout << "FizzBuzz" << "\n";
-	}
-	//if integer is a multiple of 3 send FIZZ
-	else if (integer == multiple3)
-	{
-		std::cout << "Fizz" << "\n";
-	}
-	//if integer is a multiple of 5 send BUZZ
-	else if (integer == multiple5)
-	{
-		std::cout << "Buzz " << "\n";
-	}
-	//if integer is not a multiple of 3 or 5 send the integer
-	else
-	{
-		std::cout << integer << "\n";
-	}
-
-	return 0;
-
-
-}
-int main()
-{
-	int test = 12000;
-
-
-
-	FizzBuzzFunction(test);
-
-}
+	
