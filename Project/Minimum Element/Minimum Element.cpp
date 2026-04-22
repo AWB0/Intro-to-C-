3509c3d5020a25c5bd677d1f349238f5456af643
AWB0 <theawb0@gmail.com>
2026-04-23 00:21:09 +1000

    tweaked Logic, added assert and comments

--- Project/Minimum Element/Minimum Element.cpp
+++ Project/Minimum Element/Minimum Element.cpp
@@ -3,12 +3,13 @@
 #include <iostream>
 #include <cassert>
 
-
+//function to find the minimum value of an integer array
 int ArrayFunction(const int intArray[], const int arraySize)
 {
-
+	//define the lowest number value for use in our loop
 	int lowestNumber = 0;
 
+	//loop through every value of the array and compare the current value to the previous lowest
 	for (int i = 0; i < arraySize; i++)
 	{
 		//set the initial LowestNumber to the array first number of the array
@@ -17,6 +18,8 @@ int ArrayFunction(const int intArray[], const int arraySize)
 			lowestNumber = intArray[i];
 			continue;
 		}
+
+		//compare the current lowestNumber to the new value and assign the new lowestNumber
 		if (lowestNumber > intArray[i])
 		{
 			lowestNumber = intArray[i];
@@ -27,17 +30,18 @@ int ArrayFunction(const int intArray[], const int arraySize)
 
 	}
 
-	std::cout << lowestNumber;
 
-	return 0;
+	return lowestNumber;
 }
 int main()
 {
+	//define the array and its size
 	const int arraySize = 20;
 	int intArray[arraySize] = { 67, 13, 3, 89, 43, 2, 19, 71, 5, 61, 97, 7, 37, 31, 17, 11, 83, 53, 23, 29 };
 
-
-	ArrayFunction(intArray, arraySize);
+	
+	assert(ArrayFunction(intArray, arraySize) == 2);
+	
 
 
 }
