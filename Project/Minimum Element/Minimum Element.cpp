226a0bb0cab927fefbbbc7f16b35c323148b380f
AWB0 <theawb0@gmail.com>
2026-04-23 00:13:21 +1000

    Added Main Function

--- Project/Minimum Element/Minimum Element.cpp
+++ Project/Minimum Element/Minimum Element.cpp
@@ -31,6 +31,15 @@ int ArrayFunction(const int intArray[], const int arraySize)
 
 	return 0;
 }
+int main()
+{
+	const int arraySize = 20;
+	int intArray[arraySize] = { 67, 13, 3, 89, 43, 2, 19, 71, 5, 61, 97, 7, 37, 31, 17, 11, 83, 53, 23, 29 };
 
+
+	ArrayFunction(intArray, arraySize);
+
+
+}
 
 
