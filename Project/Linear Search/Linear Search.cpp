b392a75d7f324b7b97a025304bb7121b7c357b4e
AWB0 <theawb0@gmail.com>
2026-04-23 03:52:02 +1000

    Added asserts and comments

--- Project/Linear Search/Linear Search.cpp
+++ Project/Linear Search/Linear Search.cpp
@@ -2,39 +2,47 @@
 #include <string.h>
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 
 
 
 
-
-int LinearSearch(int numberToFind)
+//linear search function
+int LinearSearch(int intArray[], const int arraySize, int numberToFind)
 {
 
 
-	std::vector<int> array;
-	for (int i = 1; i <= 1000; i++) 
-	{
-		array.push_back(i);
-	}
-	
-	int loops = 0;
+
+	//initialise integer for returning
+	int integer;
 
 	// loop through the array based on its size linearly
-	for (int i = 0; i < array.size(); i++) 
+	for (int i = 0; i < arraySize; i++) 
 	{
 
 		// when value has been found
-		if (array[i] == numberToFind) 
+		if (intArray[i] == numberToFind) 
 		{
-			std::cout << "found Value: " << i;
+			return integer = i;
 		}
+		
+		
+		
 	}
 	
-
 	return 0;
+	
 }
 int main()
 {
-	
+	//initialise array
+	const int arraySize = 20;
+	int intArray[arraySize] = { 67, 13, 3, 89, 43, 2, 19, 71, 5, 61, 97, 7, 37, 31, 17, 11, 83, 53, 23, 29 };
+
+	//asserts to check if the search works
+	assert(LinearSearch(intArray, arraySize, 3) == 2);
+	assert(LinearSearch(intArray, arraySize, 7) == 11);
+	assert(LinearSearch(intArray, arraySize, 67) == 0);
+
 }
