f03ded9f9b21627c31c69b787eb3482563c2370a
AWB0 <theawb0@gmail.com>
2026-04-22 20:45:03 +1000

    Added Code to ArrayFunction

--- Project/Print Array/Print Array.cpp
+++ Project/Print Array/Print Array.cpp
@@ -4,11 +4,31 @@
 
 
 
-//Function for printing arrays to the console
+
 const int ArrayFunction(const int intArray[], const int arraySize)
 {
 
 
+   
+    for (int i = 0; i < arraySize; i++)
+    {
+        
+        std::cout << intArray[i];
+
+       
+        if (i < arraySize - 1)
+        {
+            std::cout << " ,";
+        }
+
+    }
+
+    return 0;
 }
+
+
+
+
+
 
 
