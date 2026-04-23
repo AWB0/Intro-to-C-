d00785be973fd97d370ed08e3eae7c81580b9e25
AWB0 <theawb0@gmail.com>
2026-04-24 04:42:16 +1000

    Simplified FizzBuzz Function

--- Project/FizzBuzz/FizzBuzz.cpp
+++ Project/FizzBuzz/FizzBuzz.cpp
@@ -9,30 +9,29 @@ std::string FizzBuzzFunction(int integer)
 {
 	//define the string we will use in the loop
 	std::string text;
-	// loop through integers until we reach the current integer and check if its equal to a multiple of 5
-	for (int i = 0; i <= integer; i++)
-	{
+
+	
 		//if value is a multiple of 3 or 5 "FizzBuzz"
-		if (i % 3 == 0 && i % 5 == 0 &&integer == i)
+		if (integer % 3 == 0 && integer % 5 == 0 &&integer == integer)
 		{
 			text = "FizzBuzz";
 	    }
 		//if value is a multiple of 3 "Fizz"
-		else if (i % 3 == 0 && integer == i )
+		else if (integer % 3 == 0 && integer == integer)
 		{
 			text = "Fizz";
 		}
 		//if value is a multiple of 5 "Buzz"
-		else if (i % 5 == 0 && integer == i )
+		else if (integer % 5 == 0 && integer == integer)
 		{
 			text = "Buzz";
 		}
 		//if value is not a multiple of 3 or 5 then integer to string
-		else if (integer == i)
+		else if (integer == integer)
 		{
 			text = std::to_string(integer);
 		}
-	}
+	
 
 	return text;
 
