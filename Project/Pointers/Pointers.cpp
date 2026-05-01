ba0363a71e40e5f7c8a3ddc69bdb1f89716a2eb7
AWB0 <theawb0@gmail.com>
2026-05-01 23:21:31 +1000

    Added Pointer based Swapping Function

--- Project/Pointers/Pointers.cpp
+++ Project/Pointers/Pointers.cpp
@@ -1,26 +1,13 @@
 #pragma once
 #include <string.h>
 #include <iostream>
+#include <cassert>
 
 //swapping using pointers
-void Pointer(int firstValue, int secondValue)
+void static Pointer(int* firstValue, int* secondValue)
 {
 	//define the pointer and to a 
-	//int temp = a;
-	// a = b;
-	// b = temp;
-
-	int *pointer = &firstValue;
-	
-
-
-
-
-
-}
-int main()
-{
-
-
-
+	int temp = *firstValue;
+	*firstValue = *secondValue;
+	*secondValue = temp;
 }
