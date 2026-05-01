08afd6f215985bf3ce38304e3dd52eb6db0f18da
AWB0 <theawb0@gmail.com>
2026-05-02 00:22:11 +1000

    Added Main Function

--- Project/Pointers/Pointers.cpp
+++ Project/Pointers/Pointers.cpp
@@ -11,3 +11,19 @@ void static Pointer(int* firstValue, int* secondValue)
 	*firstValue = *secondValue;
 	*secondValue = temp;
 }
+int main()
+{
+	int a = 45;
+	int b = 12;
+	//get original values before they are swapped
+	int aO = a;
+	int bO = b;
+
+	Pointer(&a, &b);
+
+	//assert the values have been swapped successfully
+	assert(a == bO);
+	assert(b == aO);
+
+
+}
