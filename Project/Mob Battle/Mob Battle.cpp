7b9d7e665483ea7afe4527c3980a6bd74cb41887
AWB0 <theawb0@gmail.com>
2026-05-04 13:56:22 +1000

    Added Function attempt
    Still figuring out how it should be setup

--- Project/Mob Battle/Mob Battle.cpp
+++ Project/Mob Battle/Mob Battle.cpp
@@ -2,7 +2,8 @@
 #include <string.h>
 #include <iostream>
 #include <algorithm>
-#include <time.h>
+#include <random>
+#include <cmath>
 enum AttackSkill
 {
 	poison,
@@ -16,114 +17,108 @@ enum AttackSkill
 };
 struct Mob
 {
-	std::string name;
+	std::string name = "Doll";
 
-	AttackSkill attackSkill;
+	AttackSkill attackSkill = stab;
 
-	float health;
+	float health = 20;
 
-	float damageRating;
+	float damageRating = 1;
+
 };
 //Random damage 
-int Damage(int damage, int variance)
+int Damage(float damage, float damageRatio)
 {
+	std::random_device rd;
+	std::mt19937 gen(rd());
 
-	float random = rand() % variance;
-	float damageValue = damage * (1 + random);
+	if (damageRatio < 1)
+	{
+		damageRatio = damage / 100;
+	}
+	if (damageRatio >= 2)
+	{
+		damageRatio = 1.99;
+	}
+	float MinDamage = damage / damageRatio;
+	float maxDamage = damage * damageRatio;
+	std::uniform_real_distribution<>distribution(MinDamage, maxDamage);
+
+	float damageValue = distribution(gen);
 	return damageValue;
 }
 
-float Outcome(Mob attacker, Mob attacked)
+
+float Attack(Mob attacker, Mob attacked)
 {
-	float addedValue = 0;
 	float damageValue = 0;
-	switch (attacker.attackSkill)
+
+
+	damageValue = Damage(attacker.damageRating, 1.5);
+	attacked.health -= damageValue;
+	if (attacked.health <= 0)
+	{
+				attacked.health = 0;
+	}
+	std::cout << std::endl;
+	std::cout << attacker.name << " damaged " << attacked.name << " for " << damageValue << " damage!" << std::endl;
+	return 0;
+}
+// change the name of the enemy based off it's skill
+std::string printOutcome(Mob mob1)
+{
+	switch (mob1.attackSkill)
 	{
 	case poison:
-
-		for (int i = 0; i < 5; i++)
-		{
-			damageValue = Damage(attacker.damageRating, 2) * 0.15;
-			attacked.health -= damageValue;
-		}
+		mob1.name = "The Poisonous " + mob1.name;
 		break;
+
 	case stab:
 
-		damageValue = Damage(attacker.damageRating, 5) * 0.15;
-		attacked.health -= damageValue;
+		mob1.name = "The Stabby " + mob1.name;
 		break;
-	case smite:
-
-		damageValue = Damage(attacker.damageRating, 5) * 0.15;
-		attacked.health -= damageValue;
-		break;
-	case fireBall:
-
-		damageValue = Damage(attacker.damageRating, 5) * 0.15;
-		attacked.health -= damageValue;
-		for (int i = 0; i < 10; i++)
-		{
-
-			damageValue = Damage(attacker.damageRating, 5) * 0.05;
-			attacked.health -= damageValue;
-
-			std::cout << std::endl;
-			std::cout << attacked.name << " took " << damageValue << " damage from poison!";
-
-			addedValue += damageValue;
-
-		}
-		std::cout << std::endl;
-		std::cout << attacked.name << " took " << addedValue << " damage! and has " << attacked.health << " health left!" << std::endl;
-		break;
+	
 	}
-	return attacked.health;
+	return mob1.name;
 }
-
-
+//the mobs fight once
 int Battle(Mob mob1, Mob mob2)
 {
-	
-
 	int attackTimes = 100;
-
+	float damageValue = 0;
 	for (int i = 0; i < attackTimes; i++)
 	{
-			mob2.health = Outcome(mob1, mob2);
-			mob1.health = Outcome(mob2, mob1);
-	}
 
-	if (mob1.health <= 0)
-	{
-		mob1.health = 0;
+		mob1.name = printOutcome(mob1);
+		mob2.name = printOutcome(mob2);
+	
+			
 
+			damageValue = Damage(mob1.damageRating, 1.5);
+			mob1.health -= damageValue;
+			if (mob1.health <= 0)
+			{
+				mob1.health = 0;
+			}
+			std::cout << std::endl;
+			std::cout << mob1.name << " damaged " << mob2.name << " for " << damageValue << " damage!" << std::endl;
+			break;
 
-		std::cout << mob2.name << " has fallen!" << std::endl;
-		std::cout << std::endl;
-		std::cout << mob1.name << " has won the battle!" << std::endl;
+			
 
+		}
+	
 
-	}
-	if (mob2.health <= 0)
-	{
-		mob2.health = 0;
-
-
-		std::cout << mob1.name << " has fallen!" << std::endl;
-		std::cout << std::endl;
-		std::cout << mob2.name << " has won the battle!" << std::endl;
-
-	}
+	
 
 	return 0;
 }
 int main()
 {
-	srand(time(NULL));
 
-	Mob delph{ "Delph",poison, 50, 10.0f };
+	Mob delph{ "Delph",poison, 50, 10.0f};
 
-	Mob vessel = { "Vessel", stab, 100, 5.0f };
+	Mob vessel = { "Vessel", stab, 100, 5.0f};
 
 
 	Battle(delph, vessel);
